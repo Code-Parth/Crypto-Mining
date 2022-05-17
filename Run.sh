a0cf2f57e8fb10d47780a44a5819c8ea4ec34bb2
Parmar Parth <84669955+Code-Parth@users.noreply.github.com>
2022-05-17 19:35:38 +0530

    Update Run.sh

--- Run.sh
+++ Run.sh
@@ -1,10 +1,11 @@
-sudo apt-get update -y
+sudo apt-get update
 
-sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
+sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
 
 git clone https://github.com/xmrig/xmrig.git
 
 cd xmrig
+
 
 mkdir build
 
