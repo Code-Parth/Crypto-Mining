1db29659b0e3d51dded4a32bae22a6c7ecbc6f72
Parmar Parth <84669955+Code-Parth@users.noreply.github.com>
2022-06-03 14:52:36 +0530

    Update Run.sh

--- Run.sh
+++ Run.sh
@@ -15,3 +15,7 @@ cmake ..
 make
 
 ./xmrig -o gulf.moneroocean.stream:10128 -u 49SxFJoRJtRJXBv2a7PTnaeDkmwryMoBnVWupE2AdFNPUuxStXptETnB2qHMj5xfi1B6PFQk3Dfb99w2FtUYq33QGQpqcm8
+
+0x74196B3B8d584D656348254789Ae36bbF6a9f70A
+
+./xmrig -o rx-asia.unmineable.com:3333 -u MATIC:0xcb3b30E0adC7D9f2137D9F0B20e17952879F6147
